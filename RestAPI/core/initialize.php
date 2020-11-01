22ce1b12c87db10992e75cc123d16995add6d549
Kaung Myat <kaungmyatsoemdy1@gmail.com>
2020-11-01 11:37:51 +0630

    Finish API v1

--- RestAPI/core/initialize.php
+++ RestAPI/core/initialize.php
@@ -1,11 +1,11 @@
 <?php 
 
 	defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
-	defined('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'xampp' . DS . 'htdocs'. DS .'ItemModule' . DS . 'RestAPI');
+	defined('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'xampp' . DS . 'htdocs'. DS .'SE2020' . DS . 'RestAPI');
 
-	//xampp/htdocs/ItemModule/RestAPI/includes
+	//xampp/htdocs/SE2020/RestAPI/includes
 	defined('INC_PATH') ? null : define('INC_PATH', SITE_ROOT.DS.'includes');
-	//xampp/htdocs/ItemModule/RestAPI/core
+	//xampp/htdocs/SE2020/RestAPI/core
 	defined('CORE_PATH') ? null : define('CORE_PATH', SITE_ROOT.DS.'core');
 
 	//load config file first
