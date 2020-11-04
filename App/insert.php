16cf5ac40c6e9c7641c2be5a7c2ae2d4cdda0f1d
Tint Tint Htun <tinttinttun1581998@gmail.com>
2020-11-04 11:29:25 +0630

    Update Insert file

--- App/insert.php
+++ App/insert.php
@@ -2,10 +2,8 @@
 <html>
 <head>
 	<title>Add Product</title>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-
+	<title>Edit Product</title>
+	
 	<style type="text/css">
 
 		body {
@@ -27,29 +25,42 @@
  			padding: 0 0 10px 0;
  			border-bottom: 1px solid #ddd;
  			}
+			 
  		.form{
  			padding-left: 45px;
  			padding-right: 45px;
  			
  		}	
 
-
 		table{
 			padding: 10px;
 			margin: 5px;
 
 		}
+
 		th{
 			padding-left: 10px;
-			padding-top: 10px;
-			padding-bottom: 10px;
+			padding-bottom: 20px;
+			width:150px;
 			}
 
- 		input[type=text] {
-				 display: block;
- 				margin-bottom: 10px;
- 				height: 25px;
- 				}
+ 		input{
+			display: block;
+			margin-bottom: 20px;
+			height: 25px;
+			width:100%;
+		}
+		
+		textarea{
+			width:100%;
+			margin-bottom: 20px;
+		}
+
+		select{
+			height: 25px;
+			width:104%;
+			margin-bottom: 20px;
+		}
  		
  		tr{
  			text-align: left;
@@ -57,6 +68,7 @@
  		
  		button{
  			width: 80px;
+			height:30px;
  			background-color: #226089;
  			color: #ffffff;
  			border:none;
@@ -64,22 +76,7 @@
  			padding:5px;
   			
   		}
-  		.backBtn{
- 			width: 80px;
- 			background-color: #226089;
- 			color: #ffffff;
- 			border:none;
- 			border-radius: 4px;
- 			padding:5px;
-  			
-  		}
-  		.backBtn:hover{
-  			background-color:#2f89fc;
-        	font-weight:bold;
-        	color: white;
-  		}
-
-  		button:hover{
+  		.button:hover{
   			background-color:#2f89fc;
         	font-weight:bold;
   			}
@@ -88,52 +85,51 @@
  	</style>
 </head>
 <body>
-
-	<div id="wrap">
-		<form method="post" action="actions/create.php" enctype="multipart/form-data">
+    
+<div id="wrap">
+		<form >
 			<h1>Add Product</h1>
 			<div class ="form">
 				<table>
 					<tr>
-						<th width="150px" >Product Name</th>
-						<th >
-							<input type="text" name="name" class="form-control" required>
-						</th>
+						<th>Product Name</th>
+						<td ><input type="text" name="name" id="name" class="form-control" required></td>
 					</tr>
 					<tr>
-						<th>Product Photo</th>
-						<th>
-							<input type="file" name="photo" accept="image/x-png,image/jpeg" class=" file-upload form-control-file " required>
-						</th>
+                        <th><label class="control-label" for="photo">Product Photo</label></th>
+						<td>
+                        <input type="file" name="photo" accept="image/x-png,image/jpeg" id="photo" required>
+						</td>
 					</tr>
 					<tr>
 						<th >Descriptions</th>
-						<th ><textarea class="form-control" rows="3" id="descriptions" name="description" required></textarea></th>
+						<td ><textarea class="form-control" name="descriptions" id="descriptions" cols="30" rows="6" required></textarea></td>
 					</tr>
 					<tr>
 						<th >Stock Balance</th>
-						<th ><input type="number" name="stockBal" min="0" max="99999" class="form-control" required></th>
+						<td ><input type="number" name="stockBal" min="0" max="99999" class="form-control" required></td>
 					</tr>
 					<tr>
 						<th >Price</th>
-						<th ><input type="number" name="price" min="1" class="form-control" required></th>
+						<td ><input type="number" name="price" min="1" class="form-control" required></td>
 					</tr>
 					<tr>
 						<th >Warehouse</th>
-						<th >
-							<select class="form-control" name="warehouse" required >
-								<option value="">Choose...</option>
-								<?php include_once "actions/wh_retrieve.php" ?>
-						</th>
+						<td ><select class="form-control" name="warehouse" required >
+							<option value="">Choose...</option>
+							<option>1</option>
+							<option>2</option>
+							<option>3</option>
+							<option>4</option>
+							</select>
+						</td>
 					</tr>
 					<tr>
 						<th></th>
-						<th>
-							<button type="submit" class="button" name="create">
-								Submit
-							</button>
-							<a href="index.php" class="btn backBtn">Cancel</a>
-						</th>
+						<td>
+							<button type="submit" class="button">Submit</button>
+							<button type="cancel" class="button">Cancel</button>
+						</td>
 					</tr>
 				</table>
 			</div>
@@ -141,5 +137,4 @@
 	</div>
 </body>
 </html>
-
-
+        
